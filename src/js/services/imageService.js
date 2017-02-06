23dcadc4e6585ab79ed757a21ddce35ba6ad018b
cedric frossard <adrion.dev@gmail.com>
2017-02-06 15:29:31 +0100

    add image search with infinite scroll

---
+++ src/js/services/imageService.js
@@ -0,0 +1,30 @@
+'use strict';
+
+app.factory('ImageService', function($http, SELECT_SERVICE_URL) {
+
+  return {
+    getImageResults: function(query, lang, rows, page) {
+      if (!lang) {
+        lang = 'en';
+      }
+      if (!rows) {
+        rows = 4;
+      }
+
+      return $http.get(SELECT_SERVICE_URL, {
+        params: {
+          fq: 'docType:image',
+          wt: 'json',
+          indent: 'off',
+          docType: 'image',
+          group: true,
+          'group.field': 'contentMD5',
+          searchLanguage: lang,
+          rows: rows,
+          start: page * rows,
+          q: query,
+        },
+      });
+    }
+  };
+});
