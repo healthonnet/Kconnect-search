fa8e59e127988c0ffe6ea54ed7296769043d9a43
cedric frossard <adrion.dev@gmail.com>
2017-02-08 11:20:50 +0100

    keep search query to image Search

--- src/js/controllers/appController.js
+++ src/js/controllers/appController.js
@@ -1,14 +1,17 @@
 'use strict';
 
 app.controller('AppController',
-  ['$scope', '$translate', 'LANGUAGES', 'VERSION',
-  function($scope, $translate, lang, version) {
+  ['$scope', '$translate', 'LANGUAGES', 'VERSION', '$location',
+  function($scope, $translate, lang, version, $location) {
     $scope.lang = lang[$translate.proposedLanguage()];
     $scope.$on('switchLang', function(event, args) {
       $scope.lang = lang[args];
     });
     $scope.$on('$routeChangeSuccess', function(evt, absNewUrl, absOldUrl) {
       $scope.previous = absOldUrl;
+    });
+    $scope.$on('$locationChangeSuccess', function() {
+      $scope.currentQuery = $location.search().q;
     });
     $scope.version = version;
   },]);
