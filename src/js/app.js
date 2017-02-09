5b9e7d62f31e10b39551cf24eff9a728fc51ae19
Pierre Repetto-Andipatin <pierre.repetto@gmail.com>
2017-02-09 13:39:28 +0100

    feat: add visual loading bar

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 
 var app = angular.module('App', [
   'ui.bootstrap',
+  'angular-loading-bar',
   'ngRoute',
   'pascalprecht.translate',
   'ngSanitize',
--- src/js/filters/cropFilter.js
+++ src/js/filters/cropFilter.js
@@ -4,6 +4,7 @@ app.filter('crop', function() {
   return function(input) {
     // TODO: have a filter removing excess repositories
     // like something.com/a/b/c/d/e/f/g/h => something.com/a/.../h
+    // http://stackoverflow.com/questions/14594965
     return input;
   };
 });
