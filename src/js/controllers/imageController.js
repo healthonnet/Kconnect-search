fda140508d743f7b99cf9eb88456835b580e75e8
cedric frossard <adrion.dev@gmail.com>
2017-02-13 08:04:00 +0100

    fix lint (thanks github editor)

--- src/js/controllers/imageController.js
+++ src/js/controllers/imageController.js
@@ -6,7 +6,7 @@ app
   function($scope, $location, $imageService, angularGridInstance) {
     $scope.$emit('picturesActive');
     $scope.pageIcon = 'fa-picture-o';
-    
+
     function parseImageResults(res) {
       res.data.grouped.contentMD5.groups.forEach(function(group) {
         group.doclist.docs[0].actualHeight  = group.doclist.docs[0].height;
