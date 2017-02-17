69fe9632eae0049a7370347f769aa47db79c07dd
cedric frossard <adrion.dev@gmail.com>
2017-02-17 09:47:54 +0100

    add translation service and dynamic urls to translated Website

--- src/js/controllers/searchController.js
+++ src/js/controllers/searchController.js
@@ -3,24 +3,40 @@
 app.controller('SearchController',
   ['$scope', '$location', 'ResultsService', 'SuggestionService',
   'TrustabilityService', 'ScreenshotService', 'DisambiguatorService',
+  'TranslationService',
   function($scope, $location, resultsService, suggestionService,
-    trustabilityService, screenshotService, disambiguatorService) {
+    trustabilityService, screenshotService, disambiguatorService,
+    translationService) {
     $scope.pageTitle = 'Search';
     $scope.pageIcon = 'fa-globe';
     $scope.pageTitleColor = 'text-dark-blue';
     $scope.form = {};
+    $scope.translatedQueries = {};
+    $scope.targetLang = $scope.kConfig.lang;
     $scope.searchActive = true;
     $scope.filters = $scope.filters || {};
     $scope.filters = deserialize($location.search().filters);
     $scope.$emit('searchActive');
 
-    $scope.submit = function() {
+    $scope.submit = function(targetlang) {
       if ($scope.form.param !== undefined) {
         // Prevent useless submit (same request)
-        if ($scope.form.param === $location.search().q) {
+        if ($scope.form.param === $location.search().q && !targetlang) {
           return;
         }
+
         $location.search('q', $scope.form.param);
+        $location.search('lang', targetlang);
+
+        if ($scope.translatedQueries[targetlang]) {
+          $location.search('translated', $scope.translatedQueries[targetlang]);
+        }
+
+        if (targetlang === $scope.kConfig.lang || !targetlang) {
+          $location.search('lang', null);
+          $location.search('translated', null);
+        }
+
       }
     };
 
@@ -55,25 +71,67 @@ app.controller('SearchController',
       });
     };
 
+    $scope.handleResults = function() {
+
+    };
+
     var q = $location.search().q;
     var section = $location.search().section;
     var filters = $location.search().filters;
+    var translated = $location.search().translated;
+    $scope.targetLang = $location.search().lang;
+    var allowTranslate = ($scope.targetLang && translated);
 
     if (q) {
       // By default use User language.
+
       var queryLanguage = $scope.kConfig.lang;
-      
       $scope.form.param = q;
       $scope.param = q;
+      $scope.highlight = $scope.param;
       $scope.getColor = getColor;
+
+      if ($scope.lang.translatableTargets) {
+        translationService.translateMany(q, $scope.kConfig.lang,
+          $scope.lang.translatableTargets)
+          .then(function(data) {
+            data.forEach(function(langTargetResponse) {
+              var key = langTargetResponse.config.params.targetLang;
+              var translation =
+                langTargetResponse.data.translation[0].translated[0].text;
+              $scope.translatedQueries[key] = translation;
+            });
+            console.log($scope.translatedQueries);
+          });
+      }
+
       disambiguatorService.getFatheadContent(q)
         .then(function(res) {
+          if (!res.data.results[0]) { return; }
+
           $scope.fathead = {
             type: 'definition',
             title: 'Definition of ' + q,
             content: res.data.results[0].definition,
           };
+
+          if (allowTranslate) {
+            // Translate fathead
+            translationService.translate(
+              $scope.fathead.content, $scope.kConfig.lang, $scope.targetLang)
+              .then(function(data) {
+                $scope.fathead.content =
+                    data.data.translation[0].translated[0].text;
+              });
+          }
         });
+
+      // Translated content request
+      if (allowTranslate) {
+        queryLanguage = $scope.targetLang;
+        q = translated;
+        $scope.highlight = translated;
+      }
 
       // TODO Pagination
       resultsService.getResults({
@@ -99,6 +157,10 @@ app.controller('SearchController',
           // Certification
           $scope.results.groups.forEach(function(link) {
             // Handle extra datas and mutators
+            // Region free title
+            link.doclist.docs[0].title =
+                link.doclist.docs[0]['title_' + queryLanguage];
+
             // HonCode certification
             link.doclist.docs[0].isCertified = false;
             if (link.doclist.docs[0].is_certified_facet) {
