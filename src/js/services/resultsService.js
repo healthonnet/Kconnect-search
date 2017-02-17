436f1938d4eac02007b21069cf445b9c8c0aea71
Pierre Repetto-Andipatin <pierre.repetto@gmail.com>
2017-02-17 11:10:43 +0100

    fix: questions results are the same as khresmoi

--- src/js/services/resultsService.js
+++ src/js/services/resultsService.js
@@ -13,6 +13,9 @@ app.factory('ResultsService', function($http, SELECT_SERVICE_URL) {
         filters: parseFilters(options.filters),
       };
       return $http.get(SELECT_SERVICE_URL, {
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+        },
         params: {
           facet: true,
           indent: 'off',
@@ -34,7 +37,7 @@ app.factory('ResultsService', function($http, SELECT_SERVICE_URL) {
           ' readability_difficult_facet' +
           ' readability_easy_facet' +
           ' is_certified_facet',
-          q: options.query,
+          q: parseMore(options.query),
           fq: [
             'docType:html',
             options.section,
@@ -58,4 +61,51 @@ function parseFilters(filters) {
   });
   filters = filtersArray.join(' OR ');
   return filters;
+}
+
+var customHttpParamSerializer = function(params) {
+  if (!params) { return ''; }
+  var parts = [];
+  var value;
+  for (var param in params) {
+    if (params.hasOwnProperty(param)) {
+      value = params[param];
+      if (value === null || typeof value === undefined) { return; }
+      if (Array.isArray(value)) {
+        parts.concat(serializeArray(value, param));
+      } else {
+        parts.push(encodeUriQuery(param) + '=' +
+            encodeUriQuery(serializeValue(value)));
+      }
+    }
+  }
+  return parts.join('&');
+};
+
+function encodeUriQuery(val) {
+  return encodeURIComponent(val).
+    replace(/%40/gi, '@').
+    replace(/%3A/gi, ':').
+    replace(/%24/g, '$').
+    replace(/%2C/gi, ',');
+}
+
+function serializeValue(v) {
+  if (typeof v === 'object') {
+    return v instanceof Date ? v.toISOString() : v;
+  }
+  return v;
+}
+
+function serializeArray(value, param) {
+  var parts = [];
+  value.forEach(function(v) {
+    parts.push(encodeUriQuery(param)  + '=' +
+      encodeUriQuery(serializeValue(v)));
+  });
+  return parts;
+}
+
+function parseMore(query) {
+  return query.replace(/\?/g, '\\?');
 }
