5d6279aba669dcadf088b19a74a1a419f000b89e
cedric frossard <adrion.dev@gmail.com>
2017-02-24 09:46:44 +0100

    revert useless fix

--- protractor.js
+++ protractor.js
@@ -2,7 +2,6 @@
 
 exports.config = {
   specs: ['src/tests/home.js'],
-  directConnect: true,
   seleniumServerJar: './node_modules/protractor/node_modules/' +
   'webdriver-manager/selenium/selenium-server-standalone-3.1.0.jar',
 };
